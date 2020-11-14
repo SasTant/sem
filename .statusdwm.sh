eb90a7e6e7d882dfdf08f247c52089095cb8e159
Syaiful Sutanto <sas1ahad@gmail.com>
2020-11-14 13:40:49 +0700

    Tambah script u/ status dwm

---
+++ .statusdwm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+#Variabel 
+JEDA=30 #detik
+
+#Fungsi
+waktu()
+{
+    date "+D: %A %d %b | T: %H:%M"
+}
+
+suara()
+{
+    amixer scontents | awk '/Left\:\ Play/{print $5 $6}'
+}
+
+pong()
+{
+    ping 8.8.8.8 -c 1 | awk '/64/{print $7 $8}'
+}    
+
+#Mainloop
+while true; do
+    xsetroot -name "$(waktu) | V: $(suara) | I: $(pong)"
+    sleep $JEDA
+done 
