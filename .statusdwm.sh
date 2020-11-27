e9507887da5dafe0dfd205ce559adfd2af7fbd03
Syaiful Sutanto <sas1ahad@gmail.com>
2020-11-27 21:24:42 +0700

    cek level suara, muncul peringatan bila di atas 85 % statusdwm.sh

--- .statusdwm.sh
+++ .statusdwm.sh
@@ -2,7 +2,7 @@
 
 
 #Variabel 
-JEDA=30 #detik
+JEDA=15 #detik
 
 #Fungsi
 smem()
@@ -25,8 +25,19 @@ pong()
     ping 8.8.8.8 -c 1 | awk '/64/{print $7 $8}' | sed 's/time/ping/' 
 }    
 
+statok()
+{
+    xsetroot -name "$(waktu) | V: $(suara) | I: $(pong) | M: $(smem)"
+}
+
+statvol()
+{
+    xsetroot -name "----------!!! LOUD VOL !!!-----------|$(waktu) | $(suara) | I: $(pong) | M: $(smem)"
+}
+
 #Mainloop
 while true; do
-    xsetroot -name "$(waktu) | V: $(suara) | I: $(pong) | M: $(smem)"
-    sleep $JEDA
+	echo $(suara) > semvol #volume saat ini di cek file ./CEKVOL, u/ deteksi 85% ke atas
+	grep -f cekvol semvol  && statvol || statok 
+    	sleep $JEDA
 done 
