cbb78a85f6a437e9da5c35becc83fed22f4076c8
Syaiful Sutanto <sas1ahad@gmail.com>
2020-12-04 01:49:39 +0700

    baraction.sh: cpu read; spectrwm.conf:font bar change

--- baraction.sh
+++ baraction.sh
@@ -5,6 +5,20 @@
 JEDA=15 #detik
 
 #Fungsi
+cpur() {
+read cpu a b c d e f g abai < /proc/stat
+
+sleep 0.5
+
+read cpu2 a2 b2 c2 d2 e2 f2 g2 abai < /proc/stat
+
+prevtotal=$((a+b+c+e+f+g))
+total=$((a2+b2+c2+e2+f2+g2))
+cpu=$((total-prevtotal ))
+cpu=$(( 2 * cpu   ))
+echo "CPU: $cpu %"
+}
+
 smem()
 {
     free -mh | awk '/Mem/{print $3 }'
@@ -25,25 +39,14 @@ pong()
     ping 8.8.8.8 -c 1 | awk '/64/{print $7 $8}' | sed 's/time/ping/' 
 }    
 
-#cpu()
-#{
-#	read cpu a b c previdle rest < /proc/stat
-#	prevtotal=$((a+b+c+previdle))
-#	sleep 0.5
-#	read cpu a b c idle rest < /proc/stat
-#	total=$((a+b+c+idle))
-#	cpu=$((100 * (total-prevtotal) - (idle-previdle) / (total-prevtotal) ))
-#	echo "CPU: $cpu"
-#}
-
 statok()
 {
-	echo "$(waktu) | V: $(suara) | I: $(pong) | M: $(smem)"
+	echo "$(waktu) | V: $(suara) | I: $(pong) | M: $(smem) | $(cpur)"
 }
 
 statvol()
 {
-	echo "----------!!! LOUD VOL !!!-----------|$(waktu) | $(suara) | I: $(pong) | M: $(smem)" 
+	echo "----------!!! LOUD VOL !!!-----------|$(waktu) | $(suara) | I: $(pong) | M: $(smem) | $(cpur)" 
 }
 
 
